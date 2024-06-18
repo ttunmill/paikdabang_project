32e7aeb2ffd32d85da0a131130aeba42ae2f24bb
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-18 11:20:25 +0900

    paikdabang

---
+++ js/common.js
@@ -0,0 +1,17 @@
+const sub = document.querySelectorAll(".sub")
+for(var i of sub) {i.style.display = "none"}
+
+// swiper
+const swiper1 = new Swiper(".sec01_swiper", {
+    autoplay : {delay : 5000},
+    loop : true,
+    pagination: {
+        el: /* ".swiper-pagination", */'.sec01_swiper .swiper-pagination',
+        clickable: true,
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl:".swiper-button-next", 
+        prevEl:".swiper-button-prev"
+    },
+})
